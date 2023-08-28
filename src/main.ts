a71baa0ee9c3406392221d5553003efe37bd911c
Liam Tietjens <l.tietjens@gotmail.com>
2023-08-28 23:40:59 +1200

    bacics are there now !!!
    gona be a rush to finish in time but im still holding hope so better than most dit projects haha.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,21 @@
 import './style.css'
-import viteLogo from "../public/vite.svg"
+import viteLogo from "./typescript.svg"
 import navBar from './html/nav-all.html?raw'
 import footer from './html/footer-all.html?raw'
 import homeMain from './html/home-main.html?raw'
+import heroBanner from './html/hero-banner.html?raw'
 
 function HtmlDomConstruct(path: String) {
   // header content
   var navbar = navBar.replace("${viteLogo}", viteLogo)
-  var headerDOMEL = [ navbar ]
+  var headerDOMEL = [ heroBanner, navbar ]
   
   // main content
   var mainDOMEL = []
   if (path == "/") { 
     mainDOMEL.push(homeMain) 
   } else if (path == "/about") {
-    mainDOMEL.push(navBar)
+    mainDOMEL.push("<h1>hello</h1>")
   }
   
   // footer content
@@ -28,7 +29,21 @@ function HtmlDomConstruct(path: String) {
   }
 }
 
-console.log(HtmlDomConstruct('/help'))
+function navBarStick (navBar:HTMLDivElement) {
+  const scrollWatch = document.createElement('div')
+  
+  scrollWatch.setAttribute("data-scroll-watcher", "")
+  navBar.before(scrollWatch)
+
+  const navWatch = new IntersectionObserver (
+    (entries) => {
+      navBar.classList.toggle("navbar-stick", !entries[0].isIntersecting)
+    }
+  )
+  navWatch.observe(scrollWatch)
+}
+
+console.log(document.getElementById('#navBar'))
 console.log(location.pathname)
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -36,4 +51,9 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector<HTMLDivElement>('header')!.innerHTML = htmlObject.header
   document.querySelector<HTMLDivElement>('main')!.innerHTML = htmlObject.main
   document.querySelector<HTMLDivElement>('footer')!.innerHTML = htmlObject.footer
+  
+  var navBar = document.querySelector<HTMLDivElement>('#navBar')
+  if (navBar != null) { 
+    navBarStick(navBar)
+  }
 })
